81e3d178de03e024afecddcdbcd8dfde1dd61b79
Tomáš Bosák <tombos255@gmail.com>
2023-05-17 09:49:20 +0200

    backend server setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+
+// Add env variables
+require('dotenv').config();
+
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port} !`);
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true})
+
