0c0e4642d7e472180d6d6d9633117be3e296dc86
Krystof <krystofoxgamer@gmail.com>
2023-05-23 12:14:01 +0200

    x

--- frontend/src/scripts/login.js
+++ frontend/src/scripts/login.js
@@ -2,7 +2,7 @@ export const login = async (username, password) => {
     // Send login request to server
     const responseServer = await fetch('http://' + import.meta.env.VITE_BACKEND_SERVER + '/login', {
         method: 'POST',
-        body: JSON.stringify({ Username: username, Password: password }),
+        body: JSON.stringify({ username: username, password: password }),
         headers: {
             'Content-Type': 'application/json'
         }
@@ -11,6 +11,7 @@ export const login = async (username, password) => {
 
     if (response.message)
         return response.message
+    console.log(response);
     if (!response.token || response.token.length != 22)
         return "SERVER ERROR!!!"
 
