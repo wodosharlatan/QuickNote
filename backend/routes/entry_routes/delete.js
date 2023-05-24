ea37a405be7ddf26c699e17f002adbac99aa98c7
Tomáš Bosák <tombos255@gmail.com>
2023-05-24 10:13:50 +0200

    delete entry working

--- backend/routes/entry_routes/delete.js
+++ backend/routes/entry_routes/delete.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const Entry = require("../../models/entry_model");
+const User = require("../../models/user_model");
 const { AuthenticateUser } = require("../../functions");
 
 // Add env variables
 require("dotenv").config();
 
 // Delete Entry by ID
-router.post("/", async (req, res) => {
+router.post("/:ID", async (req, res) => {
 	try {
 		if ((await AuthenticateUser(req.body.token)) === false) {
 			res.json({ message: "Unauthorized" });
 			return;
 		}
 
-		await Entry.deleteOne({ ID: req.body.id });
+		const entry = await Entry.findOne({ ID: req.params.ID });
+
+		if (!entry) {
+			res.json({ message: "Entry not found" });
+			return;
+		}
+
+		const oneUser = await User.findOne({ UserToken: req.body.token });
+		Username = oneUser.Username;
+
+		// Check if the entry was created by the authenticated user
+		if (entry.AddedBy !== Username) {
+			res.json({ message: "Not authorized to delete this entry" });
+			return;
+		}
+
+		await Entry.deleteOne({ ID: req.params.ID });
 		res.json({ message: "Entry deleted !" });
 	} catch (error) {
 		res.json({ message: error.toString() });
