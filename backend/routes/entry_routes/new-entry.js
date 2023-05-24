f7e311f0666a0e78839c189c860953a5bdd36577
Tomáš Bosák <tombos255@gmail.com>
2023-05-24 11:08:17 +0200

    plz work

--- backend/routes/entry_routes/new-entry.js
+++ backend/routes/entry_routes/new-entry.js
@@ -27,7 +27,7 @@ async function generateID() {
 // Make new Entry
 router.post("/", async (req, res) => {
 	// Check if user exists
-	let addedby = "";
+	let validateUsername = "";
 
 	try {
 		if ((await AuthenticateUser(req.body.token)) === false) {
@@ -35,10 +35,10 @@ router.post("/", async (req, res) => {
 			return;
 		}
 
-		const oneUser = await User.findOne({ Username: req.body.username });
+		const oneUser = await User.findOne({ Username: req.body.addedby });
 
-		if (oneUser.Username === req.body.username) {
-			addedby = oneUser.Username;
+		if (oneUser.Username === req.body.addedby) {
+			validateUsername = oneUser.Username;
 		} else {
 			res.json({ message: "User does not exist" });
 			return;
@@ -48,53 +48,65 @@ router.post("/", async (req, res) => {
 		return;
 	}
 
-	// Check if the deadline is in the past
-	const deadline = new Date(req.body.deadline);
-	const today = new Date();
-
-	const urgency = req.body.urgency;
-	const textlength = req.body.text.trim().length;
-	const titlelength = req.body.title.trim().length;
-
-	// Check if the text and title are between 10 and 1000 characters
-	if (textlength < 10 || textlength > 1000) {
-		res.json({ message: "Text must be between 10 and 1000 characters" });
-		return;
-	}
-
-	if (titlelength < 5 || titlelength > 50) {
-		res.json({ message: "Title must be between 5 and 50 characters" });
-		return;
-	}
-
-	// check if urgency is a number between 1 and 3
-	if (urgency < 1 || urgency > 3) {
-		res.json({ message: "Urgency must be a number between 1 and 3" });
-		return;
-	}
-
-	// Check if the deadline is in the past
-	if (deadline < today) {
-		res.json({ message: "Deadline is in the past" });
-		return;
-	}
-
-	const title = req.body.title.trim();
-	const text = req.body.text.trim();
-	const ispublic = req.body.ispublic.trim();
-
-	// Create new Entry
-	const entry = new Entry({
-		ID: await generateID(),
-		Urgency: urgency,
-		DeadLine: deadline,
-		Title: title,
-		Text: text,
-		IsPublic: ispublic,
-		AddedBy: addedby,
-	});
-
 	try {
+		// Check if the deadline is in the past
+		const deadline = req.body.deadLine;
+		const parts = deadline.split(".");
+		const deadline_day = parts[0];
+		const deadline_month = parts[1];
+		const deadline_year = parts[2];
+
+		const formattedDealine = `${deadline_year}-${deadline_month}-${deadline_day}`;
+
+		const currentDate = new Date();
+		const current_year = currentDate.getFullYear();
+		const current_month = String(currentDate.getMonth() + 1).padStart(2, "0");
+		const current_day = String(currentDate.getDate()).padStart(2, "0");
+
+		const formattedDate = `${current_year}-${current_month}-${current_day}`;
+
+		const urgency = req.body.urgency;
+		const textlength = req.body.text.trim().length;
+		const titlelength = req.body.title.trim().length;
+
+		// Check if the text and title are between 10 and 1000 characters
+		if (textlength < 10 || textlength > 1000) {
+			res.json({ message: "Text must be between 10 and 1000 characters" });
+			return;
+		}
+
+		if (titlelength < 5 || titlelength > 50) {
+			res.json({ message: "Title must be between 5 and 50 characters" });
+			return;
+		}
+
+		// check if urgency is a number between 1 and 3
+		if (urgency < 1 || urgency > 3) {
+			res.json({ message: "Urgency must be a number between 1 and 3" });
+			return;
+		}
+
+		// Check if the deadline is in the past
+		if (formattedDealine < formattedDate) {
+			res.json({ message: "Deadline is in the past" });
+			return;
+		}
+
+		const title = req.body.title.trim();
+		const text = req.body.text.trim();
+		const ispublic = req.body.ispublic.trim();
+
+		// Create new Entry
+		const entry = new Entry({
+			ID: await generateID(),
+			Urgency: urgency,
+			DeadLine: new Date(formattedDealine),
+			Title: title,
+			Text: text,
+			IsPublic: ispublic,
+			AddedBy: validateUsername,
+		});
+
 		await entry.save();
 		res.json({ message: "Entry successfully created" });
 		return;
--- backend/routes/user_routes/user.js
+++ backend/routes/user_routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../models/user_model");
 const { AuthenticateAdmin } = require("../../functions");
+
 // Add env variables
 require("dotenv").config();
 
