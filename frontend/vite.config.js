72b85b230de5a3a09af1d8949135ca0c9a32fd15
Krystof <krystofoxgamer@gmail.com>
2023-05-24 08:37:20 +0200

    env fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return defineConfig({
   plugins: [vue()],
   server: {
-    port: 4000,
+    port: process.env.VITE_PORT,
   },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
+}
+
