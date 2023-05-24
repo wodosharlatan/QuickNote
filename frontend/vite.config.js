ed34133bef5f2859b3dc6913e9d2716e8b51d653
Krystof <krystofoxgamer@gmail.com>
2023-05-24 08:45:34 +0200

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig, loadEnv  } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default ({ mode }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
@@ -13,7 +12,7 @@ export default ({ mode }) => {
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@':  path.resolve(__dirname, './src'),
     }
   }
 });
